d8167938b8e811081ff5674c4425753fc7bd473d
teacherIan <ian_malloy@ssis.asia>
2023-03-09 15:05:53 +0800

    sadfdsa

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 import './style.css';
+
+//'https://randomuser.me/api/'
+
+const img = document.getElementById('img');
+console.log(img);
+
+getRandomPerson();
+async function getRandomPerson() {
+  const data = await fetch('https://randomuser.me/api/');
+  const person = await data.json();
+  img.src = person.results[0].picture.large;
+}
